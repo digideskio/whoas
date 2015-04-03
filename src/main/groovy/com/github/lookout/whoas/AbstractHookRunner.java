1b44cd2c1eb0eb043ec2f7bb86e50d45f3498876
R. Tyler Croy <tyler@monkeypox.org>
2015-04-03 07:31:53 -0700

    Move AbstractHook into Java

---
+++ src/main/groovy/com/github/lookout/whoas/AbstractHookRunner.java
@@ -0,0 +1,38 @@
+package com.github.lookout.whoas;
+
+
+/**
+ *
+ */
+public abstract class AbstractHookRunner {
+    protected AbstractHookQueue queue;
+    protected Publisher publisher;
+    protected Boolean keepGoing = true;
+
+    public AbstractHookRunner(AbstractHookQueue hookQueue) {
+        this(hookQueue, new Publisher());
+    }
+
+    public AbstractHookRunner(AbstractHookQueue hookQueue,
+                              Publisher hookPublisher) {
+        this.publisher = hookPublisher;
+        this.queue = hookQueue;
+    }
+
+    public Publisher getPublisher() {
+        return this.publisher;
+    }
+
+    /** Block forever and run the runner's runloop. */
+    public abstract void run();
+
+    /**
+     * Tell the runloop to stop
+     *
+     * This will only come into effect after the runner has completed it's
+     * currently executing work
+     */
+    public void stop() {
+        this.keepGoing = false;
+    }
+}
